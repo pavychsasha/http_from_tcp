2cd40460f1a8e9a4820c389f2154354d6e9febf9
spavych <spavych@gmail.com>
2026-02-02 23:14:58 -0600

    Modifying internal parser

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,12 +3,15 @@ package headers
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 type Headers map[string]string
 
 const CRLF = "\r\n"
+
+func NewHeaders() Headers {
+	return Headers{}
+}
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
@@ -17,35 +20,39 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	case -1:
 		return 0, false, nil
 	case 0:
-		return 1, true, nil
+		// the empty line
+		// headers are done, consume the CRLF
+		return 2, true, nil
 	}
 
-	headersFull := string(data[:crlfIdx])
-	key, value, err := parseHeaderString(headersFull)
+	key, value, err := parseHeaderBytes(data[:crlfIdx])
 	if err != nil {
 		return 0, false, fmt.Errorf("Could not parse headers string: %v", err)
 	}
-	h[key] = value
 
+	h.Set(string(key), string(value))
 	return crlfIdx + 2, false, nil
 
 }
 
-func parseHeaderString(val string) (key string, value string, err error) {
+func (h Headers) Set(key, value string) {
+	h[key] = value
+}
 
-	values := strings.SplitN(val, ":", 2)
+func parseHeaderBytes(val []byte) (key []byte, value []byte, err error) {
+
+	values := bytes.SplitN(val, []byte(":"), 2)
 	if len(values) < 2 {
-		return "", "", fmt.Errorf("Invalid header format: headers should be in the following format:\nfield-line   = field-name ':' OWS field-value OWS\nGiven: %s", val)
+		return []byte{}, []byte{}, fmt.Errorf("Invalid header format: headers should be in the following format:\nfield-line   = field-name ':' OWS field-value OWS\nGiven: %s", val)
 	}
 	key = values[0]
 	value = values[1]
 
-	if string(key[len(key)-1]) == " " {
-		return "", "", fmt.Errorf("Invalid header: there should not be any spaces between field-name and colon in %s", val)
+	if key[len(key)-1] == byte(' ') {
+		return []byte{}, []byte{}, fmt.Errorf("Invalid header: there should not be any spaces between field-name and colon in %s", val)
 	}
-	fmt.Println(key, " ", value)
 
-	key = strings.ReplaceAll(key, " ", "")
-	value = strings.ReplaceAll(value, " ", "")
+	key = bytes.ReplaceAll(key, []byte(" "), []byte(""))
+	value = bytes.ReplaceAll(value, []byte(" "), []byte(""))
 	return key, value, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,10 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func NewHeaders() Headers {
-	return Headers{}
-}
-
 func TestHeadersParse(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
@@ -21,6 +17,38 @@ func TestHeadersParse(t *testing.T) {
 	assert.Equal(t, "localhost:42069", headers["Host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
+
+	// Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("Host:   localhost:42069    \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 29, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with extra whitespace
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	data = []byte("Host-2:     localhost:3232069    \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	assert.Equal(t, "localhost:3232069", headers["Host-2"])
+	assert.Equal(t, 35, n)
+	assert.False(t, done)
+
+	// Test: valid Done
+	data = []byte("\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
